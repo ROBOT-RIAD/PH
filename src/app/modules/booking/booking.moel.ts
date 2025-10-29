80e956600d90d004f84bd2a5aea2b5c27aa3034c
ROBOT-RIAD <riadhosain420@gmail.com>
2025-10-29 17:56:15 +0600

    update code

--- src/app/modules/booking/booking.moel.ts
+++ src/app/modules/booking/booking.moel.ts
@@ -6,7 +6,7 @@ import { BOOKING_STATUS, IBooking } from "./booking.interface";
 const bookingSchema = new Schema<IBooking>({
     user : {type : Schema.Types.ObjectId , ref : "User", required : true},
     tour : {type : Schema.Types.ObjectId , ref : "Tour", required : true},
-    payment : {type : Schema.Types.ObjectId , ref : "Payment", required : true},
+    payment : {type : Schema.Types.ObjectId , ref : "Payment"},
     status : {type :String , enum : Object.values(BOOKING_STATUS), default : BOOKING_STATUS.PENDING},
     guestCount : { type : Number , required : true}
 },{
